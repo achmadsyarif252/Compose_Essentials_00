8bd25a79ce037a11f1aba3c345007804bfe3c88a
achmadsyarif252 <19kaizen@gmail.com>
2023-10-26 19:38:31 +0700

    menambahkan animasi extraPadding by animateDpAsState dan rememberSaveable untuk handle configuration changes(rotation,theme mode,etc)

--- app/src/main/java/com/syarif/jetpackcompose00/MainActivity.kt
+++ app/src/main/java/com/syarif/jetpackcompose00/MainActivity.kt
@@ -5,19 +5,29 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,24 +39,111 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackCompose00Theme {
-                Text(text = "Hello World")
+                MyApp()
             }
         }
     }
 }
 
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnBoarding) { // Where does this come from?
+            OnboardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier, onContinueClicked: () -> Unit) {
+    //TODO : This state should be hoisted
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked,
+        ) {
+            Text(text = "Continue")
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        LazyColumn {
+            items(names) { name ->
+                Greeting(name = name)
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp, label = "", animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow,
+        )
+    )
+
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
+            }
+        }
+
+    }
+}
 
 @Preview(name = "Light Mode")
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
 )
-@Preview
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun GreetingsPreview() {
+    JetpackCompose00Theme {
+        Greetings()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun PreviewMessageCard() {
     JetpackCompose00Theme {
-        Surface {
-//            MyApp()
-        }
+        MyApp(Modifier.fillMaxSize())
     }
 }
 
